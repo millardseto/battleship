2287a02e29e7a3ed4407de118941915730fb0bb2
Millard Seto <millardseto@mac.com>
2017-04-24 22:01:53 -0700

    Adds index and js files

---
+++ js/battleship.js
@@ -0,0 +1,40 @@
+/* Head First JavaScript Ch02  */
+
+var randomLoc = Math.floor(Math.random() * 5); // random 0 to 4
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim fire! (enter a number 0-6):");
+
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses++;
+    }
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+        alert("HIT!");
+        hits++;
+
+        if (hits == 3) {
+            isSunk = true;
+            alert("you sank my battleship!");
+        }
+
+    } else {
+        alert("MISS");
+    }
+
+
+} // end loop
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+    "which means your shooting accuracy was " + (3 / guesses);
+alert(stats);
